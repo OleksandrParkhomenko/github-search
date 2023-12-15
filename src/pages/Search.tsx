933470352b53a95796b229769b9255d469560593
Alex Parkhomenko <ole.parkhomenko@gmail.com>
2023-12-15 20:19:10 +0200

    Remove SearchContainer

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,16 +1,21 @@
 // src/pages/Search.tsx
 import React from 'react';
-import SearchContainer from '../containers/SearchContainer/SearchContainer';
 import Typography from '@mui/material/Typography';
 import styles from '../styles/common.module.css';
+import useSearch from '../hooks/useSearch';
+import SearchResults from '../components/SearchResults/SearchResults';
+import SearchBar from '../components/SearchBar/SearchBar';
 
 const Search: React.FC = () => {
+  const { searchQuery, setSearchQuery, repositories, loading, hasMore, loadMore, called } = useSearch();
+  
   return (
     <div className={styles.container}>
       <Typography variant="h1" gutterBottom>
         Search
       </Typography>
-      <SearchContainer />
+      <SearchBar value={searchQuery} onChange={setSearchQuery} loading={loading} />
+      <SearchResults loading={loading} repositories={repositories} hasMore={hasMore} onLoadMore={loadMore} called={called} />
     </div>
   );
 };
